23b163326e060f79ecd23c656ef32e0950d9e1e5
marcotherx <marcoandrejr@gmail.com>
2024-06-17 22:22:48 -0300

    inimigos prontos

--- levels/scripts/enemy.gd
+++ levels/scripts/enemy.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 1580.0
+const SPEED = 2670.0
 const JUMP_VELOCITY = -400.0
 
 @onready var wall := $wall as RayCast2D
 @onready var texture := $texture as Sprite2D
-
+@onready var anim := $anim as AnimationPlayer
 var direction := 1
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -28,3 +28,11 @@ func _physics_process(delta: float) -> void:
 	velocity.x = direction * SPEED * delta
 
 	move_and_slide()
+
+
+
+
+
+func _on_anim_animation_finished(anim_name):
+	if anim_name == "hurt":
+		queue_free()
--- levels/scripts/player.gd
+++ levels/scripts/player.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_FORCE = -400.0
+const JUMP_FORCE = -350.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
 
 @onready var animacaof := $anime as AnimatedSprite2D
-
+@onready var remote_transform := $Remote as RemoteTransform2D
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -35,3 +35,12 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animacaof.play("idle")
 	move_and_slide()
+
+
+func _on_hurtbox_body_entered(body):
+	if body.is_in_group("enemies"):
+		queue_free()
+		
+func follow_camera(camera):
+	var camera_path = camera.get_path()
+	remote_transform.remote_path = camera_path
